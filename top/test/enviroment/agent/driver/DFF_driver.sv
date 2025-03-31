8ae2746bd43e7ae19974a9fbbd21ef014316c69d
Ali Adel <aliadelmahdi77@gmail.com>
2025-03-31 03:14:02 +0200

    updated the driver

--- top/test/enviroment/agent/driver/DFF_driver.sv
+++ top/test/enviroment/agent/driver/DFF_driver.sv
@@ -22,12 +22,9 @@ package DFF_driver_pkg;
             forever begin
                 stimulus_seq_item = DFF_seq_item::type_id::create("stimulus_seq_item");
                 seq_item_port.get_next_item(stimulus_seq_item);
-
                 dff_if.rst = stimulus_seq_item.rst;
                 dff_if.en = stimulus_seq_item.en;
                 dff_if.d = stimulus_seq_item.d;
-                dff_if.q = stimulus_seq_item.q;
-
                 @(negedge dff_if.clk)
                 seq_item_port.item_done();
                 `uvm_info("run_phase",stimulus_seq_item.sprint(),UVM_HIGH)
